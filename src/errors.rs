764ddf68208f47310652cbeecefd2eae6945e640
Temkinnn <artyomyaskel@gmail.com>
2026-07-28 00:31:02 +0300

    Create custom error type

---
+++ src/errors.rs
@@ -0,0 +1,31 @@
+use actix_web::{HttpResponse, ResponseError, http::StatusCode};
+use serde_json::json;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Database Error")]
+    Database(#[from] sqlx::Error),
+
+    #[error("Resource not found")]
+    NotFound,
+
+    #[error("Unauthorized")]
+    Unathorized,
+}
+
+impl ResponseError for AppError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            AppError::Database(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::NotFound => StatusCode::NOT_FOUND,
+            AppError::Unathorized => StatusCode::UNAUTHORIZED,
+        }
+    }
+
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code()).json(json!({
+            "error": self.to_string()
+        }))
+    }
+}
